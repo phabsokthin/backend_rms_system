a7120fe429abc5e4f685d6947bd2e4c26e8b84dd
sok thin <phabsokthin992003@gmail.com>
2026-03-19 13:48:42 +0700

    update json add core

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connectDB();
 
 // app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5001", // allow only this origin
+  origin: "*", // allow only this origin
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // optional: specify allowed methods
   credentials: true // optional: allow cookies
 }));
